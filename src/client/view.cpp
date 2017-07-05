a6e2bcc354a278d9513acca54678e29eb78ee67c
Evan <esmit121@vols.utk.edu>
2017-07-05 08:40:56 -0400

    Changed slider/progress bar update behavior

--- src/client/view.cpp
+++ src/client/view.cpp
@@ -10,10 +10,8 @@ Window::Window(QWidget * Parent)
 {
 	model = new Model((void*) this, "testRecord");
 
-/* Connect the view's static callback wrapper function to the model */
+// Connect the view's static callback wrapper function to the model
 	model->setCallback(&WrapperToCallback);
-
-	setMinimumSize(300, 150);
 
 	int rangeLow = (int) model->getRangeMin();
 	int rangeHigh = (int) model->getRangeMax();
@@ -22,23 +20,30 @@ Window::Window(QWidget * Parent)
 	progress_bar = new QProgressBar(this);
 	progress_bar->setRange(rangeLow, rangeHigh);
 	progress_bar->setValue(value);		
-/* Get rid of the '%' in the progress bar's text */
+// Get rid of the '%' in the progress bar's text.
 	progress_bar->setFormat("%p");
 
 	slider = new QSlider(this);
 	slider->setOrientation(Qt::Horizontal);
 	slider->setRange(rangeLow, rangeHigh);
 	slider->setValue(value);
+	slider->setTracking(false);						// To prevent spamming the updateModelValue() call, we do not enable slider tracking
+													// This means that only the final position of the slider will emit a 'value changed'
+													// signal. The updateModelValue() involves a computationally expensive network exchange.
 
-/* Connect the slider widget to the progress bar widget */
-	QObject::connect(slider, SIGNAL (valueChanged(int)), progress_bar, SLOT (setValue(int)));
-/* Connect the slider widget to the view's updateModelValue() function call. */
-	QObject::connect(slider, SIGNAL (valueChanged(int)), this , SLOT (updateModelValue(int)));
-/* Connect the view's value changed signal to the update function */
+// Connect the slider widgets position to the progress bar widgets value.
+	QObject::connect(slider, SIGNAL (sliderMoved(int)), progress_bar, SLOT (setValue(int)));
+
+// Connect the slider widget's 'value changed' to the view's updateModelValue() function call.
+	Object::connect(slider, SIGNAL (valueChanged(int)), this , SLOT (updateModelValue(int)));
+
+// Connect the view's 'value changed' signal to the updateViewValue() function call.
 	QObject::connect(this, SIGNAL (valueChanged(int)), this, SLOT (updateViewValue(int)));
 
-/* Formatting main window */
+// Formatting main window
 
+	setMinimumSize(300, 150);
+	
 	QBoxLayout * mainLayout = new QBoxLayout(QBoxLayout::TopToBottom);
 
 	mainLayout->addWidget(progress_bar);
@@ -49,38 +54,36 @@ Window::Window(QWidget * Parent)
 	setWindowTitle(tr("qtEpics slider client"));
 }
 
-/* Calls the model's putValue() method. This writes a new value to the record held
- * on the server.
- * */
+// Calls the model's putValue() method. This writes a new value to the record held
+// on the server.
 void Window::updateModelValue(const int & value) {
 	model->putValue(value);
 }
 
-/* Static wrapper function to window's callback member function */
+// Static wrapper function to window's callback member function
 void Window::WrapperToCallback(void * ptrToObj, const int & value)
 {
 	Window * window = (Window *) ptrToObj;
 	window->callback(value);
 }
 
-/* Updates value of view when model is updated by its monitor */
+// Updates value of view when model is updated by its monitor
 void Window::updateViewValue(const int & value)
 {
-	/* Block signal emition. This prevents signal loops when 'setValue' is called */
+	// Block signal emition. This prevents signal loops when 'setValue' is called
 	blockSignals(true);
 
-	/* Update widget values based on the new value from the model */
+	// Update widget values based on the new value from the model
 	slider->setValue(value);
 	progress_bar->setValue(value);
 	
-	/* Unblock signal emition */
+	// Unblock signal emition
 	blockSignals(false);
 }
 
-/* Emit valueChanged. This is used instead of calling 'setValue' on the widgets directly
- * becuase it is called from a seperate thread. This allows the main event thread to 
- * handle repaints 
- * */
+// Emit valueChanged. This is used instead of calling 'setValue' on the widgets directly
+// becuase it would be called from a seperate thread. This allows the main event thread to 
+// handle repaints 
 void Window::callback(const int & value)
 {
 	emit valueChanged(value);
