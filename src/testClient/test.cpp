cf2ed0c216f9efe2d0e9d9741e036e71e3c3fcd1
Evan <esmit121@vols.utk.edu>
2017-07-10 14:17:06 -0400

    added generator functions for input. added interation and interval functionality.

--- src/testClient/test.cpp
+++ src/testClient/test.cpp
@@ -1,9 +1,50 @@
 #include "test.h"
+
 #include <pv/pvaClient.h>
+
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 using namespace std;
+using namespace epics::pvData;
 using namespace epics::pvaClient;
+
+long genLong()
+{
+	return (RAND_MAX / 2) + (rand() % RAND_MAX);
+}
+
+
+double genDouble() 
+{
+	double f = (genLong() % 10000000) + ((double)rand()) / INT_MAX;
+	return f;
+}
+
+// Generate pseudo random alphanumeric input.
+string genString()
+{
+	// String size between 10 and 50
+	size_t str_len = (rand() % 41) + 10;
+	
+	string str;
+	str.resize(str_len + 1);
+	
+	for (size_t i = 0; i < str_len; ++i) {
+		// ascii characters 33 -> 126 are printable
+		// not including space which is 32.
+		str[i] = (rand()%94) + 33;
+	}
+
+	return str;
+}
+
+// Not reall fifty fifty. Thus fittyfitty.
+bool fittyfitty()
+{
+	return rand() % 2;
+}
 
 int test(const string & channelName,
 		  const bool & debug,
@@ -13,6 +54,8 @@ int test(const string & channelName,
 
 	try {
 		
+		srand(time(NULL));
+
 		PvaClientPtr pva = PvaClient::get();
 
 		if (debug) PvaClient::setDebug(debug);
@@ -25,9 +68,31 @@ int test(const string & channelName,
 		}
 	
 		PvaClientPutPtr put = channel->createPut("");
-		PvaClientPutDataPtr putData = put->getData();
+		PVStructurePtr pvStr = put->getData()->getPVStructure();
 
-		putData->getPVStructure()->dumpValue(cout);
+		for (size_t i = 0; i < (unsigned) iterations; ++i) {
+			
+			struct timespec rem;
+			struct timespec req;
+
+			pvStr->getSubField<PVLong>("longInteger1")->put(genLong());
+			pvStr->getSubField<PVLong>("longInteger2")->put(genLong());
+			
+			pvStr->getSubField<PVDouble>("double1")->put(genDouble());
+			pvStr->getSubField<PVDouble>("double2")->put(genDouble());
+		
+			pvStr->getSubField<PVString>("string1")->put(genString());
+			pvStr->getSubField<PVString>("string2")->put(genString());
+
+			pvStr->getSubField<PVBoolean>("boolean")->put(fittyfitty());
+
+			put->put();
+		
+			req.tv_sec = (int) interval;
+			req.tv_nsec = (interval - (int) interval) * 1000000000;
+			
+			nanosleep(&req, &rem);
+		}
 
 	} catch (std::runtime_error err) {
 		cerr << err.what() << endl;
