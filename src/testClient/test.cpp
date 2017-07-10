d20f9de98c4c5d1bfc04b8c2ff5ed3394b5a51d7
Evan <esmit121@vols.utk.edu>
2017-07-10 14:28:12 -0400

    comments and formatting.

--- src/testClient/test.cpp
+++ src/testClient/test.cpp
@@ -10,12 +10,13 @@ using namespace std;
 using namespace epics::pvData;
 using namespace epics::pvaClient;
 
+// Generate a pseudorandom integer value.
 long genLong()
 {
 	return (RAND_MAX / 2) + (rand() % RAND_MAX);
 }
 
-
+// Generate a pseudorandom floating point value;
 double genDouble() 
 {
 	double f = (genLong() % 10000000) + ((double)rand()) / INT_MAX;
@@ -25,31 +26,31 @@ double genDouble()
 // Generate pseudo random alphanumeric input.
 string genString()
 {
-	// String size between 10 and 50
+	// String size is between 10 and 50.
 	size_t str_len = (rand() % 41) + 10;
 	
 	string str;
 	str.resize(str_len + 1);
 	
 	for (size_t i = 0; i < str_len; ++i) {
-		// ascii characters 33 -> 126 are printable
-		// not including space which is 32.
+		// ascii characters 33 -> 126 are printable,
+		// not including 'space' which is 32.
 		str[i] = (rand()%94) + 33;
 	}
 
 	return str;
 }
 
-// Not reall fifty fifty. Thus fittyfitty.
+// Not really fifty fifty odds. Thus fittyfitty.
 bool fittyfitty()
 {
 	return rand() % 2;
 }
 
 int test(const string & channelName,
-		  const bool & debug,
-		  const int & iterations,
-		  const double & interval)
+		 const bool & debug,
+		 const int & iterations,
+		 const double & interval)
 {
 
 	try {
@@ -72,8 +73,8 @@ int test(const string & channelName,
 
 		for (size_t i = 0; i < (unsigned) iterations; ++i) {
 			
-			struct timespec rem;
-			struct timespec req;
+			struct timespec req; // Requested time to sleep.
+			struct timespec rem; // Remaining time to sleep.
 
 			pvStr->getSubField<PVLong>("longInteger1")->put(genLong());
 			pvStr->getSubField<PVLong>("longInteger2")->put(genLong());
@@ -88,7 +89,9 @@ int test(const string & channelName,
 
 			put->put();
 		
+			// Cast to truncate the decimal value off. 
 			req.tv_sec = (int) interval;
+			// Convert the decimal value to nanoseconds.
 			req.tv_nsec = (interval - (int) interval) * 1000000000;
 			
 			nanosleep(&req, &rem);
--- src/testClient/test.h
+++ src/testClient/test.h
@@ -1,8 +1,20 @@
+
+// test.h
+
+// Defines a test function to be used exclusively with 'testRecord'
+
+/* Use of this function with a record with a different structure than that of 'testRecord'
+ * as defined in server/record.cpp will cause a segmentation fault.
+ * */
+
 #ifndef TEST_H
 #define TEST_H
 
 #include <string>
 
+/* Opens a channel using pvAccess to the record specified by channelName. It will then
+ * write to the record 'iterations' times, seperated by 'interval seconds.
+ * */
 int test(const std::string & channelName,
 		 const bool & debug,
 		 const int & iterations,
